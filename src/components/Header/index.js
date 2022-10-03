92f0aa51e5b61620c66eb978bad37436bcec3fab
Surya T N <suryatej.nim@yahoo.com>
2022-10-03 17:33:31 +0000

    Implemented JSX elements to render the intended UI for Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,31 @@
-// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="header-navbar">
+    <div className="header-brand-container">
+      <img
+        className="header-brand-image"
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        alt="wave"
+      />
+      <p className="header-text">Wave</p>
+    </div>
+
+    <ul className="header-links-list">
+      <li className="header-link header-text">
+        <Link to="/">Home</Link>
+      </li>
+
+      <li className="header-link header-text">
+        <Link to="/about">About</Link>
+      </li>
+
+      <li className="header-link header-text">
+        <Link to="/contact">Contact</Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Header
