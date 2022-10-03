d226f765a0dc60b63c7c46b1be901abe2f49df5d
Surya T N <suryatej.nim@yahoo.com>
2022-10-03 17:43:29 +0000

    Implemented JSX elements to render intended UI layout of About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,17 @@
-// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-content-container">
+    <img
+      className="about-content-image"
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+    />
+    <h1 className="about-content-header">About</h1>
+    <p className="about-content-description">
+      I love to create! I am a frontend web developer
+    </p>
+  </div>
+)
+
+export default About
