95d9bcbf415ed5a584a6130b87fe95f52513815b
Surya T N <suryatej.nim@yahoo.com>
2022-10-03 17:53:44 +0000

    Implemented JSX elements to render intended UI layout of Home, NotFound components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,14 @@
-// Write your JS code here
+import './index.css'
+
+const Home = () => (
+  <div className="home-content-container">
+    <img
+      className="home-content-image"
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+    />
+    <h1 className="contact-content-header">Home</h1>
+  </div>
+)
+
+export default Home
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,14 @@
-// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-content-container">
+    <img
+      className="not-found-content-image"
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="not found"
+    />
+    <h1 className="not-found-content-header">Not Found</h1>
+  </div>
+)
+
+export default NotFound
