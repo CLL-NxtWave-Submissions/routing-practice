551db4b46b0db4f9a418e67051e13f63ae40b84c
Surya T N <suryatej.nim@yahoo.com>
2022-10-03 17:16:40 +0000

    Implemented JSX elements to render the intended UI layout of the whole app in the App component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
-const App = () => <h1>Remove this heading and write your code here</h1>
+import {Route, Switch} from 'react-router-dom'
+
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+
+const App = () => (
+  <div className="wave-bg-container">
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route component={NotFound} />
+    </Switch>
+  </div>
+)
 
 export default App
