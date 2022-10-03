226ebbb7892af97f36ab4b5e1613d5654cd1b0f5
Surya T N <suryatej.nim@yahoo.com>
2022-10-03 17:47:36 +0000

    Implemented JSX elements to render intended UI layout of Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,1 +1,14 @@
-// Write your JS code here
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-content-container">
+    <img
+      className="contact-content-image"
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="contact"
+    />
+    <h1 className="contact-content-header">Contact</h1>
+  </div>
+)
+
+export default Contact
